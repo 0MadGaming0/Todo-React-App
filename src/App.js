b69435de7f2b9d9423dc19e946513219bf0b38a5
0MadGaming0 <madhavsp05@gmail.com>
2024-08-06 22:49:59 +0530

    Todo React App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddTodo from './components/AddTodo';
+import SearchTodo from './components/SearchTodo';
+import ViewallTodo from './components/ViewallTodo';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<AddTodo/>}/>
+      <Route path="/search" element={<SearchTodo/>}/>
+      <Route path="/view" element={<ViewallTodo/>}/>
     </Routes>
     </BrowserRouter>
   );
---
+++ src/components/ViewallTodo.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const ViewallTodo = () => {
+const [data,changeData]=useState([])
+const getDataFromApi= ()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(
+        (Todo)=>{
+           changeData(Todo.data)
+        }
+    ).catch()
+}
+
+   useEffect(()=>{getDataFromApi()},[])
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">UserID</th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Completed Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value,index)=>{
+                                            return <tr>
+                                            <th scope="row">{value.userId}</th>
+                                            <td>{value.id}</td>
+                                            <td>{value.title}</td>
+                                            <td>{value.completed}</td>
+                                        </tr>
+                                        }
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewallTodo
